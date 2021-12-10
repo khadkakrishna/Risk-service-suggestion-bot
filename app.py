9c6bd02548398fb3cbde9fe593812bf88014f415
khadkakrishna <krishnakhadka2802@gmail.com>
2021-12-10 12:12:53 +0530

    Adding rules page with some sample rules

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ app = Flask(__name__)
 def home(): 
     return render_template('index.html')
 
+@app.route("/policies")
+def policies(): 
+    return render_template('policies.html')
+
+@app.route("/rules")
+def rules(): 
+    return render_template('rules.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
